f57f0122f736d7c868f4f2ef1ca38677af6a82af
tommy <tommy860925@gmail.com>
2024-01-25 18:05:12 +0800

    test.php for testing

--- v1/asus/test.php
+++ v1/asus/test.php
@@ -7,7 +7,7 @@ $serverCommunicator = new ServerCommunicator();
 
 // Example: Fetch data for a specific time period
 $startTime = '2024-01-20 00:00:00';
-$endTime = '2024-01-23 16:29:00';
+$endTime = '2024-01-25 16:00:00';
 $data = $serverCommunicator->dataWithTimePeriod($startTime, $endTime);
 
 if ($data) {
