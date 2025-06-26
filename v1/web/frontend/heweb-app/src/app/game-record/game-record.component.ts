4191275a75392ca1802087f1bae1aa9b11ef2446
xwpi22 <xwpi22@gmail.com>
2025-06-26 16:19:45 +0800

    add game record below bulletin board on the web

--- v1/web/frontend/heweb-app/src/app/game-record/game-record.component.ts
+++ v1/web/frontend/heweb-app/src/app/game-record/game-record.component.ts
@@ -3,20 +3,33 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from '../api-service.service';
 import { Record } from '../models/patient.model';
 import { NgIf, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-game-record',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './game-record.component.html',
   styleUrl: './game-record.component.css'
 })
 
 export class GameRecordComponent {
-  @Input() userId?: string;     // 從父元件傳入
+  @Input() userId?: string;
   records: Record[] = [];
+  filteredRecords: Record[] = [];
+
+  gameIds: number[] = [];
+  selectedGameId: string = '';
+  gameSelected: boolean = false;
+
   loading = false;
   error: string | null = null;
+
+  gameList = [
+    { id: '0', name: '數字點點名' },
+    { id: '1', name: '五顏配六色' },
+    { id: '2', name: '按鈕排排站' },
+  ];
 
   constructor(private api: ApiService) {}
 
@@ -29,9 +42,12 @@ export class GameRecordComponent {
   private fetchRecords(userId: string) {
     this.loading = true;
     this.error = null;
+
     this.api.getRecords(userId).subscribe({
       next: (recs) => {
-        this.records = recs;
+        this.records = recs.reverse();
+        this.filteredRecords = [];
+        this.gameIds = Array.from(new Set(recs.map(r => r.gameId))); // 去重後的 Game ID
         this.loading = false;
       },
       error: (err) => {
@@ -40,5 +56,15 @@ export class GameRecordComponent {
         this.loading = false;
       }
     });
+  }
+
+  onSelectGame(gameId: string) {
+    this.selectedGameId = gameId;
+    this.gameSelected = true;
+    if (gameId === '') {
+      this.filteredRecords = [];
+    } else {
+      this.filteredRecords = this.records.filter(r => r.gameId.toString() === gameId);
+    }
   }
 }
--- v1/web/frontend/heweb-app/src/main.ts
+++ v1/web/frontend/heweb-app/src/main.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
